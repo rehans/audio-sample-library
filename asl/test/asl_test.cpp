185718c5d807c70d0d1ec5db7b52fd16213626fd
Hanzano <hanzano@web.de>
2013-04-13 15:19:23 +0200

    Added write_wave_file, Changed comments and variable names

--- asl/test/asl_test.cpp
+++ asl/test/asl_test.cpp
@@ -4,11 +4,12 @@
 //	http://www.boost.org/LICENSE_1_0.txt)
 
 #include "wave_io.h"
+#include "aiff_io.h"
 #include <algorithm>
 
-/*!	@fnc Test silence wav file
+/*!	@fn test_read_wave_silence
 */
-bool test_silence ()
+bool test_read_wave_silence ()
 {
 	int32_t expected_riff_chunk_size = 88236;
 	int32_t expected_data_chunk_size = 88200;
@@ -17,19 +18,43 @@ bool test_silence ()
 	};
 
 	asl::wave_chunk wav;
-	asl::load_wave_file("silence_1_sec_mono_16-bit_PCM.wav", wav);
+	asl::read_wave_file("silence_1_sec_mono_16-bit_PCM.wav", wav);
 
 	bool cmp_result =	wav.chunk_size == expected_riff_chunk_size && 
-						memcmp (&wav.fmt, &expected_fmt_chunk, sizeof (expected_fmt_chunk)) == 0 && 
+						memcmp (&wav.format, &expected_fmt_chunk, sizeof (expected_fmt_chunk)) == 0 && 
 						wav.data.chunk_size == expected_data_chunk_size && 
 						wav.data.chunk_size == wav.data.buffer.size ();
 
 	return cmp_result;
 }
 
+/*!	@fn test_write_wave_silence
+*/
+bool test_write_wave_silence ()
+{
+	asl::wave_chunk wav;
+	asl::read_wave_file("silence_1_sec_mono_16-bit_PCM.wav", wav);
+	asl::write_wave_file("silence_1_sec_mono_16-bit_PCM_test.wav", wav);
+
+	asl::wave_chunk wav2;
+	memset (&wav2, 0, sizeof (wav2));
+	asl::read_wave_file("silence_1_sec_mono_16-bit_PCM_test.wav", wav2);
+
+	bool cmp_result = wav.chunk_size == wav2.chunk_size && 
+		memcmp (&wav.format, &wav2.format, sizeof (wav2.format)) == 0 && 
+		wav.data.chunk_size == wav2.data.chunk_size && 
+		wav.data.chunk_size == wav2.data.buffer.size ();
+
+	return cmp_result;
+}
+
+/*!	@fn main
+*/
 int main(int argc, char** argv)
 {
-	bool test_result = test_silence ();
+	bool test_result = true;
+	test_result = test_result && test_read_wave_silence ();
+	test_result = test_result && test_write_wave_silence ();
 
 	return test_result == true ? 0 : 1;
 }
--- asl/wave_io.h
+++ asl/wave_io.h
@@ -3,8 +3,8 @@
 //	(See accompanying file LICENSE_1_0.txt or copy at
 //	http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef __asl_wav_io__
-#define __asl_wav_io__
+#ifndef __asl_wav_file_reader__
+#define __asl_wav_file_reader__
 
 #include <fstream>
 #include <stdint.h>
@@ -14,48 +14,51 @@
 namespace asl
 {
 static const char* riff_chunk_ID	= "RIFF";
-static const char* riff_type		= "WAVE";
+static const char* wave_type		= "WAVE";
 static const char* format_chunk_ID	= "fmt ";
 static const char* data_chunk_ID	= "data";
+
+static const uint16_t sizeof_format_chunk = 16; //! by specification 16 bytes
 
 //------------------------------------------------------------------------
 /*!	@struct riff_chunk
 	@brief Structure of the RIFF WAVE file format
-	
-	The WAVE format is actually a special version of the RIFF standard.
+
+	WAVE requires two sub chunks "fmt " and "data".
 */
 //------------------------------------------------------------------------
 struct wave_chunk
 {
-	uint32_t chunk_size;	//! riff chunk size
+	uint32_t chunk_size;	//! wave_chunk size
 	char riff_type[4];		//! riff type e.g. "WAVE" in this case
 	
 	struct format_chunk
 	{
-		uint32_t chunk_size;	//! format chunk size
+		uint32_t chunk_size;	//! format_chunk size is 16 bytes by specification for PCM
 		uint16_t audio_format;	//! audio format e.g. 0x0001 is PCM
 		uint16_t channel_count;	//! number of channels
 		uint32_t sample_rate;	//! the sample rate e.g. 44100
-		uint32_t byte_rate;		//!
-		uint16_t block_align;	//!
+		uint32_t byte_rate;		//! TODO
+		uint16_t block_align;	//! TODO
 		uint16_t bits_per_sample;//! bits per sample e.g. 16
-	} fmt;
+		//! There can be some more in case it is not PCM
+	} format;
 	
 	struct data_chunk
 	{
-		uint32_t chunk_size;	//! data chunk size
-		std::vector<uint8_t> buffer;	//! buffer with e.g. PCM data
+		uint32_t chunk_size;	//! data_chunk size
+		std::vector<uint8_t> buffer;//! buffer with e.g. PCM data
 	} data;
 };
 
 //------------------------------------------------------------------------
-/*!	@fn load_wave_file
-	@brief Loads an audiofile of type wav
-	@param file_name The path including name of the file
-	@param riff The resulting RIFF chunk handed in as a reference
+/*!	@fn read_wave_file
+	@brief reads an audio file of type wav
+	@param file_name Name of the file to load
+	@param wave Result of the reading
 */
 //------------------------------------------------------------------------
-void load_wave_file (const char* file_name, wave_chunk& riff)
+void read_wave_file (const char* file_name, wave_chunk& wave)
 {
 	std::ifstream file_stream;
 	file_stream.open(file_name, std::ifstream::binary);
@@ -66,24 +69,24 @@ void load_wave_file (const char* file_name, wave_chunk& riff)
 		file_stream.read(chunk_ID, sizeof(chunk_ID));
 		if (std::strncmp (chunk_ID, riff_chunk_ID, sizeof(chunk_ID)) == 0)
 		{
-			file_stream.read (reinterpret_cast<char*>(&riff.chunk_size), sizeof (riff.chunk_size));
-			file_stream.read (riff.riff_type, sizeof (riff.riff_type));
+			file_stream.read (reinterpret_cast<char*>(&wave.chunk_size), sizeof (wave.chunk_size));
+			file_stream.read (wave.riff_type, sizeof (wave.riff_type));
 			while (file_stream.good ())
 			{
 				file_stream.read(chunk_ID, sizeof(chunk_ID));
 				if (std::strncmp (chunk_ID, format_chunk_ID, sizeof(chunk_ID)) == 0)
 				{
-					file_stream.read (reinterpret_cast<char*>(&riff.fmt), sizeof (wave_chunk::format_chunk));
-					if (riff.fmt.chunk_size > sizeof_format_chunk)
+					file_stream.read (reinterpret_cast<char*>(&wave.format), sizeof (wave_chunk::format_chunk));
+					if (wave.format.chunk_size > sizeof_format_chunk)
 					{
-						file_stream.ignore (riff.fmt.chunk_size - sizeof_format_chunk);
+						file_stream.ignore (wave.format.chunk_size - sizeof_format_chunk);
 					}
 				}
 				else if (std::strncmp (chunk_ID, data_chunk_ID, sizeof(chunk_ID)) == 0)
 				{
-					file_stream.read (reinterpret_cast<char*>(&riff.data.chunk_size), sizeof (riff.data.chunk_size));
-					riff.data.buffer.resize (riff.data.chunk_size);
-					file_stream.read (reinterpret_cast<char*>(&riff.data.buffer[0]), riff.data.chunk_size);
+					file_stream.read (reinterpret_cast<char*>(&wave.data.chunk_size), sizeof (wave.data.chunk_size));
+					wave.data.buffer.resize (wave.data.chunk_size);
+					file_stream.read (reinterpret_cast<char*>(&wave.data.buffer[0]), wave.data.chunk_size);
 				}
 				else
 				{
@@ -99,6 +102,37 @@ void load_wave_file (const char* file_name, wave_chunk& riff)
 }
 
 //------------------------------------------------------------------------
+/*!	@fn write_wave_file
+	@brief writes a wave chunk to file
+	@param file_name Name of the file to write
+	@param wave Chunk to be written
+*/
+//------------------------------------------------------------------------
+void write_wave_file (const char* file_name, const wave_chunk& wave)
+{
+	std::ofstream file_stream;
+	file_stream.open(file_name, std::ifstream::binary);
+	if (file_stream.is_open())
+	{
+		//!	RIFF
+		file_stream.write (riff_chunk_ID, sizeof (riff_chunk_ID));
+		file_stream.write (reinterpret_cast<const char*>(&wave.chunk_size), sizeof (wave.chunk_size));
+		file_stream.write (wave_type, sizeof (wave_type));
+
+		//! fmt
+		file_stream.write (format_chunk_ID, sizeof (format_chunk_ID));
+		file_stream.write (reinterpret_cast<const char*>(&wave.format), sizeof (wave.format));
+
+		//! data
+		file_stream.write (data_chunk_ID, sizeof (data_chunk_ID));
+		file_stream.write (reinterpret_cast<const char*>(&wave.data.chunk_size), sizeof (wave.data.chunk_size));
+		file_stream.write (reinterpret_cast<const char*>(&wave.data.buffer[0]), wave.data.buffer.size ());
+
+		file_stream.close ();
+	}
+}
+
+//------------------------------------------------------------------------
 } // namespace asl
 
-#endif // __asl_wav_io__
+#endif // __asl_wav_file_reader__
