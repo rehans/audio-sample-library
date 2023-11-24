fd75533e0b571267d325a4c35ef077f13b0b7a13
René Hansen <rene@hansen-audio.de>
2023-11-24 09:29:52 +0100

    Extract WaveChunk type

---
+++ include/asl/wave_def.h
@@ -0,0 +1,52 @@
+// Copyright(c) 2023 rehans.
+
+#pragma once
+
+#include <stdint.h>
+#include <vector>
+
+namespace asl {
+
+//------------------------------------------------------------------------
+static const uint32_t CHUNK_ID_SIZE              = 4;
+static const char RIFF_CHUNK_ID[CHUNK_ID_SIZE]   = {'R', 'I', 'F', 'F'};
+static const char WAVE_TYPE[CHUNK_ID_SIZE]       = {'W', 'A', 'V', 'E'};
+static const char FORMAT_CHUNK_ID[CHUNK_ID_SIZE] = {'f', 'm', 't', ' '};
+static const char DATA_CHUNK_ID[CHUNK_ID_SIZE]   = {'d', 'a', 't', 'a'};
+
+static const uint16_t sizeof_format_chunk = 16; //! by specification 16 bytes
+
+//------------------------------------------------------------------------
+/*!	@struct riff_chunk
+    @brief Structure of the RIFF WAVE file format
+
+    WAVE requires two sub chunks "fmt " and "data".
+*/
+//------------------------------------------------------------------------
+struct WaveChunk
+{
+    uint32_t chunk_size;           //! WaveChunk size
+    char riff_type[CHUNK_ID_SIZE]; //! riff type e.g. "WAVE" in this case
+
+    struct FormatChunk
+    {
+        uint32_t chunk_size;      //! FormatChunk size is 16 bytes by
+                                  //! specification for PCM
+        uint16_t audio_format;    //! audio format e.g. 0x0001 is PCM
+        uint16_t channel_count;   //! number of channels
+        uint32_t sample_rate;     //! the sample rate e.g. 44100
+        uint32_t byte_rate;       //! TODO
+        uint16_t block_align;     //! TODO
+        uint16_t bits_per_sample; //! bits per sample e.g. 16
+        //! There can be some more in case it is not PCM
+    } format;
+
+    struct DataChunk
+    {
+        uint32_t chunk_size;         //! DataChunk size
+        std::vector<uint8_t> buffer; //! buffer with e.g. PCM data
+    } data;
+};
+
+//------------------------------------------------------------------------
+} // namespace asl
--- include/asl/wave_io.h
+++ include/asl/wave_io.h
@@ -1,8 +1,8 @@
 // Copyright(c) 2023 rehans.
 
-#ifndef __asl_wav_file_reader__
-#define __asl_wav_file_reader__
+#pragma once
 
+#include "wave_def.h"
 #include <cstring>
 #include <fstream>
 #include <stdint.h>
@@ -10,45 +10,6 @@
 #include <vector>
 
 namespace asl {
-static const uint32_t CHUNK_ID_SIZE              = 4;
-static const char RIFF_CHUNK_ID[CHUNK_ID_SIZE]   = {'R', 'I', 'F', 'F'};
-static const char WAVE_TYPE[CHUNK_ID_SIZE]       = {'W', 'A', 'V', 'E'};
-static const char FORMAT_CHUNK_ID[CHUNK_ID_SIZE] = {'f', 'm', 't', ' '};
-static const char DATA_CHUNK_ID[CHUNK_ID_SIZE]   = {'d', 'a', 't', 'a'};
-
-static const uint16_t sizeof_format_chunk = 16; //! by specification 16 bytes
-
-//------------------------------------------------------------------------
-/*!	@struct riff_chunk
-    @brief Structure of the RIFF WAVE file format
-
-    WAVE requires two sub chunks "fmt " and "data".
-*/
-//------------------------------------------------------------------------
-struct WaveChunk
-{
-    uint32_t chunk_size;           //! WaveChunk size
-    char riff_type[CHUNK_ID_SIZE]; //! riff type e.g. "WAVE" in this case
-
-    struct FormatChunk
-    {
-        uint32_t chunk_size;      //! FormatChunk size is 16 bytes by
-                                  //! specification for PCM
-        uint16_t audio_format;    //! audio format e.g. 0x0001 is PCM
-        uint16_t channel_count;   //! number of channels
-        uint32_t sample_rate;     //! the sample rate e.g. 44100
-        uint32_t byte_rate;       //! TODO
-        uint16_t block_align;     //! TODO
-        uint16_t bits_per_sample; //! bits per sample e.g. 16
-        //! There can be some more in case it is not PCM
-    } format;
-
-    struct DataChunk
-    {
-        uint32_t chunk_size;         //! DataChunk size
-        std::vector<uint8_t> buffer; //! buffer with e.g. PCM data
-    } data;
-};
 
 //------------------------------------------------------------------------
 /*!	@fn read_wave_stream
@@ -167,5 +128,3 @@ void write_wave_file(const char* file_name, const WaveChunk& wave)
 
 //------------------------------------------------------------------------
 } // namespace asl
-
-#endif // __asl_wav_file_reader__
